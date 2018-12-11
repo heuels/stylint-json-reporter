a522b03c1b9f09c62b55a24c1a81cedfd5fd7eca
Egor Zaitsev <zaytsevev@profi.ru>
2018-12-11 21:34:21 +0300

    feat: update reporter to work with latest stylint

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 'use strict';
-
-var path     = require('path');
 var messages = [];
 
-module.exports = function (message, done) {
+module.exports = function(message, done) {
     if (done === 'done') {
-        var result = [];
-
-        result.push({
-            filePath    : path.relative(process.cwd(), this.cache.file),
-            messages    : messages,
-            errorCount  : this.cache.errs.length,
-            warningCount: this.cache.warnings.length
-        });
-
-        this.cache.msg = JSON.stringify(result);
-
+        this.cache.msg = JSON.stringify(messages);
         return this.done();
     }
 
     messages.push({
-        severity: this.state.severity,
-        message : message,
-        rule    : this.cache.rule,
-        line    : this.cache.lineNo,
-        column  : this.cache.col
+        path: this.cache.file,
+        line: this.cache.lineNo,
+        columm: this.cache.col || 0,
+        severity: this.state.severity === 'Warning' ? 'warning' : 'failure',
+        ruleId: this.cache.rule,
+        message,
     });
 
     return '';
